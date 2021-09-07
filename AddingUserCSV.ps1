e8de86e3f1a76d719c20e13f71b71a6d9cf33460
ttreece01 <90270141+ttreece01@users.noreply.github.com>
2021-09-07 16:49:29 -0400

    Add user from CSV
    Adding users from CSV file. Path must be added and some editing might be needed to meet your specific needs.

---
+++ AddingUserCSV.ps1
@@ -0,0 +1,11 @@
+﻿#Adding users from a csv file. Must insert path of csv file and edit based on needs.
+$Newusers = Import-Csv -Path C:\
+ForEach ($u in $Newusers) {
+    $path = "OU=" + $u.Department + ",DC=Adatum,DC=com"
+    $upn = $u.UserID + "@adatum.com"
+    $display = $u.First + " " + $u.Last
+    Write-Host "Creating the following users: $display in $path"
+    New-ADUser -GivenName $u.First -Surname $u.Last -Name $display -DisplayName $display -SamAccountName $u.UserID -UserPrincipalName $UPN -Path $path -Department $u.Department
+    Write-Host "New users have been created! Please review the accounts."
+}
+
