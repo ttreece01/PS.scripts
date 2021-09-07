702a938e7aea4e16d1cd1371629c6271bee055ae
ttreece01 <90270141+ttreece01@users.noreply.github.com>
2021-09-07 18:35:34 -0400

    Sessions and file relocation
    Simple scripts to use as starting point.

---
+++ FolderandFileRelocation.ps1
@@ -0,0 +1,9 @@
+﻿#copying and moving files and folders to new location. Files and folders will now be in both areas.
+#By using the recurse command all sub folders are included.
+
+$Location1 = Read-Host "Please put path of folder you wish to copy to new location here"
+$Location2 = Read-Host "Plese enter the path to the new location here"
+
+Get-ChildItem $Location1 -Recurse | Copy-Item -Destination $Location2
+
+Write-Host "Items have been copied to new location!"
---
+++ PSRemoteSessions.ps1
@@ -0,0 +1,10 @@
+﻿#Enable Psremote and establishing a session.
+Enable-PSRemoting -Force
+
+$comp = Read-Host "Enter computer name here"
+$session = New-PSSession -ComputerName $comp
+#$comp can be replaced by computer names seperatted with commas if establishing multiple sessions.
+Get-PSSession 
+# shows current active sessions
+Get-PSSession | Remove-PSSession
+#Run above command seperate once ready to close all sessions
