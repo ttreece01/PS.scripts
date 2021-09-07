a3860855d98a6d205af42832f41c0e2d03ca927a
ttreece01 <90270141+ttreece01@users.noreply.github.com>
2021-09-07 15:59:59 -0400

    Logs from user input
    User input includes what log, which computer, and number of entries.

---
+++ LogsFromInput.ps1
@@ -0,0 +1,9 @@
+﻿#logs with user input
+
+
+[string]$logName = Read-Host "Please enter log name"
+[string]$CompName = Get-ADComputer -Filter * | Select-Object -ExpandProperty Name | Out-GridView -OutputMode Multiple
+[int]$Entries = Read-Host "How many new entries would you like to view?" 
+
+
+Get-EventLog -LogName $logName -ComputerName $CompName -Newest $Entries
